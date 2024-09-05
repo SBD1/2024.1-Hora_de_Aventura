89a18aba90d16523857e55fdbfc945112c6ef0e9
ciro-c <ciro.fga@gmail.com>
2024-09-05 03:09:46 -0300

    Fixed ddl execution & script

--- src/db/seed.py
+++ src/db/seed.py
@@ -1,4 +1,4 @@
-from db.connection import Connection
+from connection import Connection
 
 import os
 
@@ -31,7 +31,7 @@ def create_tables():
             # print("Tabelas criadas com sucesso!")
             
              # Insere dados
-            execute_sql_script(cursor, dml_arquivo)
+            # execute_sql_script(cursor, dml_arquivo)
             # print("Dados inseridos com sucesso!")
         except Exception as e:
             print(f"Erro ao criar tabelas: {e}")
@@ -44,7 +44,7 @@ def create_tables():
 def execute_sql_script(cursor, script_path):
     with open(script_path, 'r') as file:
         sql_script = file.read()
-        
+
     # Divide o script em comandos individuais
     sql_commands = sql_script.split(';')
 
@@ -52,6 +52,7 @@ def execute_sql_script(cursor, script_path):
         # Ignora comandos vazios
         if command.strip():
             try:
+                print(command)
                 cursor.execute(command)
             except Exception as e:
                 print(f"Erro ao executar comando: {e}")
