3db68860ed5da3b97850fb5ea3b09d90b5ec6e7e
luckx98 <lucasmacedob@gmail.com>
2024-09-05 00:38:31 -0300

    feat: Adicao de arquivo nicial de classes do projeto

---
+++ src/classes.py
@@ -0,0 +1,9 @@
+
+class Jogador:
+    def __init__(self, id_jogador, nome, regiao, missao_atual, vida, qnt_ouro):
+        self.id_jogador = id_jogador
+        self.nome = nome
+        self. regiao = regiao
+        self.missao_atual = missao_atual
+        self.vida = vida
+        self.qnt_ouro = qnt_ouro
